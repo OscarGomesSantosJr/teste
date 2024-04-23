48e447349ffe9f2beaf75aee6a5f0e0f9697876f
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2024-04-23 15:17:51 -0300

    Aula 23-04

--- src/controller/UsuarioController.js
+++ src/controller/UsuarioController.js
@@ -1,14 +1,18 @@
-const Usuario = require("../models/Usuario");
+const Usuario = require("../model/Usuario");
 
 module.exports = {
     async cadastrarUsuario(request, response){
-        const usuario = new Usuario(request.body);
-        if(!usuario.nome || !usuario.email || !usuario.senha){
-            return response.status(401).json({ error: 'Usuário incompleto, preencha todos os campos' });
-        }
-        else {
-            const cadastrar = await usuario.cadastrar();
-            return response.json(cadastrar);
-        }
+        
     },
 };
+
+/*
+const usuario = new Usuario(request.body);
+    if(!usuario.nome || !usuario.email || !usuario.senha){
+        return response.status(401).json({ error: 'Usuário incompleto, preencha todos os campos' });
+    }
+    else {
+        const cadastrar = await usuario.cadastrar();
+        return response.json(cadastrar);
+    }
+*/
