7e7fe88eeee3ee78f2a0e44dbb3a7c0bbe8eb499
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2024-04-25 16:40:41 -0300

    Atualização 25-04

--- knex.js
+++ knex.js
@@ -1,11 +1,11 @@
 module.exports = {
     client: 'postgresql',
     connection: {
-      host :     '',
-      port :     '',
-      database : '',
-      user :     '',
-      password : ''
+      host :     'localhost',
+      port :     '5432',
+      database : 'teste',
+      user :     'postgres',
+      password : '123456'
     },
     pool: {
       min: 2,
--- src/app.js
+++ src/app.js
@@ -2,34 +2,38 @@ const express = require('express');
 const conectar = require("./database/conexao");
 
 const app = express();
+const routes = express.Router();
 
-app.get('/home', (req, res) =>{
+routes.get('/home', (req, res) =>{
     res.contentType("Content-Type/html");
     res.status(200).send('<h1>BOM DIA</h1>');
 })
 
-app.get('/usuario', (requ, res) => {
-    const usuario = [
-        {
-            nome: "Jonas",
-            idade: "41",
-        },
-        {
-            nome: "Carlos Silva",
-            idade: "80",
+routes.post('/gravador', (request, response) => {
+    const form = async () => {
+
+        console.log(request);
+
+        try {
+            conectar('usuario').insert({
+                nome:'Pedro', 
+                idade:'18'
+        
+        }).then(()=> {
+            console.log('Dados inseridos na tabela"usuario" com sucesso:');
+        }).catch (error => {
+            console.error('Erro ao inserir dados:', error)
+        });
+        
+        } 
+        catch (error) {
+            
         }
-    ]
+    }
 })
 
 const port = '8080';
 
 app.listen(port, () => console.log(`Rodando na porta ${port} !!`));
-
-try {
-    
-} 
-catch () {
-    
-}
 
 module.exports = app;
