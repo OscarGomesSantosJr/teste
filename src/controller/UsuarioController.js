1a7622a164e3dbd9cafad225e8d97adc3845d6bb
Oscar Gomes <dsbamba@gmail.com>
2024-08-10 12:26:57 -0300

    atualização

--- src/controller/UsuarioController.js
+++ src/controller/UsuarioController.js
@@ -2,6 +2,8 @@ const Usuario = require("../model/Usuario");
 
 module.exports = {
   async cadastrarUsuario(request, response) {
+    
+    console.log(request.body);
     const usuario = new Usuario(request.body);
     const cadastrar = await usuario.cadastrar();
     
